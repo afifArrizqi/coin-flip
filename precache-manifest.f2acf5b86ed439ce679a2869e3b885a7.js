41492e0c035b35fc024b0511aff4af1c5e956060
pippop21 <afifar38@gmail.com>
2020-07-18 04:01:08 +0700

    Updates

---
+++ precache-manifest.f2acf5b86ed439ce679a2869e3b885a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92e1c821c8093754a6cf04477e814cd",
+    "url": "/coin-flip/index.html"
+  },
+  {
+    "revision": "7048823f2d03c3508239",
+    "url": "/coin-flip/static/css/main.fba36964.chunk.css"
+  },
+  {
+    "revision": "002eeeac4e26498e5eb1",
+    "url": "/coin-flip/static/js/2.2ea0ee73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coin-flip/static/js/2.2ea0ee73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7048823f2d03c3508239",
+    "url": "/coin-flip/static/js/main.64e33d24.chunk.js"
+  },
+  {
+    "revision": "e8b70a576d0b3f69541d",
+    "url": "/coin-flip/static/js/runtime-main.7843c0ab.js"
+  }
+]);
